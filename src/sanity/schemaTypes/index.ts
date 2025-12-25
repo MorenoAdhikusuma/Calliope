3359adba385b8df3c309c9733a2eb14e1e1ab3ec
MorenoAdhikusuma <redlotusgaming553@gmail.com>
2025-12-25 23:12:13 +0700

    fix : name casing problem in Sanity

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
-import galleryPage from "./GalleryPageTemp";
-import aboutPage from "./AboutPageTemp";
+import galleryPage from "./GalleryPage";
+import aboutPage from "./AboutPage";
 import BlogPost from "./BlogPost";
 
 // Add new schema types to this array as you create them
